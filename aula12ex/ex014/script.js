e24949d4e0e90171603a0457a0e725050a4f8b46
Alberto Caio <alberto1993muniz@gmail.com>
2023-05-17 22:51:01 -0300

    Termino ex2

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,20 +2,21 @@ function carregar (){
     var msg = window.document.getElementById(`msg`)
     var img = window.document.getElementById(`imagem`)
     var data = new Date()
-   // var hora = data.getHours()
-    var hora = 21
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if(hora >= 0 && hora < 12){
-        //BOM DIA
         img.src = 'manha.png'
-        document.body.style.background = `#f4e7d7`
+        document.body.style.background = `#e3ccb3`
+        msg.innerHTML = `Bom dia, agora são ${hora} horas!`
     }else if(hora >= 12 && hora < 18){
         //BOA TARDE
         img.src = 'tarde.png'
-        document.body.style.background = `#e0a65c`
+        document.body.style.background = `#dfa156`
+        msg.innerHTML = `Boa tarde, agora são ${hora} horas!`
     }else{
         //BOA NOITE
         img.src = 'noite.png'
         document.body.style.background = `#0266a0`
+        msg.innerHTML = `Boa noite, agora são ${hora} horas!`
     }
 }
