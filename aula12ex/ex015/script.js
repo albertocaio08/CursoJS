042b3be41805d6b6b47d099bd20c5fb48431fe9b
Alberto Caio <alberto1993muniz@gmail.com>
2023-05-26 01:11:44 -0300

    ex015 curso JS

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,12 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if(fano.value.length <= 0 || Number(fano.value) > ano ){
+        window.alert('[ERRO] Verificar os dados e tente novamente!')
+    }else{
+        window.alert('Você não digitou')
+    }
+}
